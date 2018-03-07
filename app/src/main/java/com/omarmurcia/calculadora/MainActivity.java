293d5f05320e117d357ee22cf4988247f1543457
omar3331 <omar.murcia@udea.edu.co>
2018-03-07 17:50:02 -0500

    Version 2.0

--- app/src/main/java/com/omarmurcia/calculadora/MainActivity.java
+++ app/src/main/java/com/omarmurcia/calculadora/MainActivity.java
@@ -3,6 +3,7 @@ package com.omarmurcia.calculadora;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 //import android.text.InputType;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,8 +12,8 @@ public class MainActivity extends AppCompatActivity {
 
     EditText ePantalla;
     String Valor;
-    int  Punto=0, cont=0, cont2=0;
-    float Resultado=0;
+    int  Punto=0, cont=0, cont2=0, cont3=0, igual=0;
+    double Resultado=0, Res=0;
     Button bPunto, bIgual, bDivision, bMultiplicacion, bResta, bSuma;
     Button bCero, bUno, bDos, bTres, bCuatro, bCinco, bSeis, bSiete, bOcho, bNueve;
 
@@ -40,11 +41,22 @@ public class MainActivity extends AppCompatActivity {
         bNueve = findViewById(R.id.bNueve);
 
         ePantalla.setText("0");
-        //ePantalla.setInputType(InputType.TYPE_NULL);
     }
 
     public void Click(View view) {
         int id = view.getId();
+
+        if (igual == 1){
+            if ((id == R.id.bCero) || (id == R.id.bUno) || (id == R.id.bDos) || (id == R.id.bTres) || (id == R.id.bCuatro) || (id == R.id.bCinco) || (id == R.id.bSeis) || (id == R.id.bSiete) || (id == R.id.bOcho) || (id == R.id.bNueve)){
+                ePantalla.setText("");
+                igual=0;
+                Resultado = 0;
+                Punto = 0;
+                cont = 0;
+                cont2 = 0;
+                cont3 = 0;
+            }
+        }
 
         Valor = ePantalla.getText().toString();
 
@@ -52,11 +64,15 @@ public class MainActivity extends AppCompatActivity {
             switch (id) {
                 case R.id.bPunto:
                     ePantalla.setText(Valor + ".");
-                    Punto++;
+                    Punto=1;
                     break;
                 case R.id.bCero:
                     if (cont != 0) {
                         ePantalla.setText(Valor + "0");
+                        Resultado = (Resultado * 10);
+                    }else {
+                        ePantalla.setText("0");
+                        Resultado = 0;
                     }
                     break;
                 case R.id.bUno:
@@ -140,52 +156,153 @@ public class MainActivity extends AppCompatActivity {
                         Resultado = 9;
                     }
                     break;
+
             }
-            cont++;
+            cont = 1; // ingresa el primer valor limpiando el anterior
         }
         else{
-            cont2 = cont2 + 10;
+            cont2 = cont2 + 10; // hace la operacion cuando son decimales
             switch (id) {
                 case R.id.bCero:
                     ePantalla.setText(Valor + "0");
                     break;
                 case R.id.bUno:
                     ePantalla.setText(Valor + "1");
-                    Resultado = Resultado + (1/cont2);
+                    Resultado = Resultado + (double)(1.0/cont2);
                     break;
                 case R.id.bDos:
                     ePantalla.setText(Valor + "2");
-                    Resultado = Resultado + (2/cont2);
-                    break;
+                    Resultado = Resultado + (double)(2.0/cont2);
+                   break;
                 case R.id.bTres:
                     ePantalla.setText(Valor + "3");
-                    Resultado = Resultado + (3/cont2);
+                    Resultado = Resultado + (double)(3.0/cont2);
                     break;
                 case R.id.bCuatro:
                     ePantalla.setText(Valor + "4");
-                    Resultado = Resultado + (4/cont2);
+                    Resultado = Resultado + (double)(4.0/cont2);
                     break;
                 case R.id.bCinco:
                     ePantalla.setText(Valor + "5");
-                    Resultado = Resultado + (5/cont2);
+                    Resultado = Resultado + (double)(5.0/cont2);
                     break;
                 case R.id.bSeis:
                     ePantalla.setText(Valor + "6");
-                    Resultado = Resultado + (6/cont2);
+                    Resultado = Resultado + (double)(6.0/cont2);
                     break;
                 case R.id.bSiete:
                     ePantalla.setText(Valor + "7");
-                    Resultado = Resultado + (7/cont2);
+                    Resultado = Resultado + (double)(7.0/cont2);
                     break;
                 case R.id.bOcho:
                     ePantalla.setText(Valor + "8");
-                    Resultado = Resultado + (8/cont2);
+                    Resultado = Resultado + (double)(8.0/cont2);
                     break;
                 case R.id.bNueve:
                     ePantalla.setText(Valor + "9");
-                    Resultado = Resultado + (9/cont2);
+                    Resultado = Resultado + (double)(9.0/cont2);
                     break;
             }
+        }
+        switch (id){
+            case R.id.bSuma:
+                if (cont3 == 0) {
+                    ePantalla.setText(Valor + "+");
+                    Res = Resultado;
+                    Resultado = 0;
+                    Punto = 0;
+                    cont2 = 0;
+                    cont3 = 1;
+                    igual = 0;
+                }
+                break;
+            case R.id.bResta:
+                if (cont3 == 0) {
+                    ePantalla.setText(Valor + "-");
+                    Res = Resultado;
+                    Resultado = 0;
+                    Punto = 0;
+                    cont3 = 2;
+                    igual = 0;
+                }
+                break;
+            case R.id.bMultiplicacion:
+                if (cont3 == 0) {
+                    ePantalla.setText(Valor + "*");
+                    Res = Resultado;
+                    Resultado = 0;
+                    Punto = 0;
+                    cont3 = 3;
+                    igual = 0;
+                }
+                break;
+            case R.id.bDivision:
+                if (cont3 == 0) {
+                    ePantalla.setText(Valor + "/");
+                    Res = Resultado;
+                    Resultado = 0;
+                    Punto = 0;
+                    cont3 = 4;
+                    igual = 0;
+                }
+                break;
+            case R.id.bIgual:
+                igual=1;
+                if (cont3 == 1) {
+                    Resultado = Resultado + Res;
+                    if (Resultado % 1 ==0){
+                        ePantalla.setText(String.format("%.0f",Resultado));
+                    }else {
+                        ePantalla.setText(String.format("%.1f", Resultado));
+                    }
+                    Res = 0;
+                    Punto = 0;
+                    cont2 = 0;
+                    cont3 = 0;
+                }else if (cont3 == 2){
+                    Resultado = Res - Resultado;
+                    if (Resultado % 1 ==0){
+                        ePantalla.setText(String.format("%.0f",Resultado));
+                    }else {
+                        ePantalla.setText(String.format("%.1f", Resultado));
+                    }
+                    Res = 0;
+                    Punto = 0;
+                    cont2 = 0;
+                    cont3 = 0;
+                }else if (cont3 == 3){
+                    Resultado = Res * Resultado;
+                    if (Resultado % 1 ==0){
+                        ePantalla.setText(String.format("%.0f",Resultado));
+                    }else {
+                        ePantalla.setText(String.format("%.1f", Resultado));
+                    }
+                    Res = 0;
+                    Punto = 0;
+                    cont2 = 0;
+                    cont3 = 0;
+                }else if (cont3 == 4){
+                    Resultado = Res / Resultado;
+                    if (Resultado % 1 ==0){
+                        ePantalla.setText(String.format("%.0f",Resultado));
+                    }else {
+                        ePantalla.setText(String.format("%.1f", Resultado));
+                    }
+                    Res = 0;
+                    Punto = 0;
+                    cont2 = 0;
+                    cont3 = 0;
+                }
+                break;
+            case R.id.bAc:
+                ePantalla.setText("0");
+                Resultado = 0;
+                Punto = 0;
+                cont = 0;
+                cont2 = 0;
+                cont3 = 0;
+                igual = 0;
+                break;
         }
     }
 }
